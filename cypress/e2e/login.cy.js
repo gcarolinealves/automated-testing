96ed8244abec36af72533bb9a390bce8e37ab5fc
Caroline Alves <e.carolinesalves@gmail.com>
2025-05-20 23:07:31 -0300

    Automated Testing

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress"/>
+
+describe("Login", () => {
+    beforeEach(() => {
+        cy.viewport("iphone-xr")
+        cy.visit("https://automationpratice.com.br/login")
+    })
+
+    it("Successful login", () => {
+        cy.get("#user").type("testing@testing.com")
+        cy.get("#password").type("123456")
+        cy.get("#btnLogin").click()
+        cy.get(".swal2-title").should("be.visible")
+        cy.get(".swal2-title").should("have.text", "Login Ok")
+    });
+
+    it("Invalid email", () => {
+        cy.get("#user").type("testing.com")
+        cy.get("#password").type("123456")
+        cy.get("#btnLogin").click()
+        cy.get(".invalid_input").should("be.visible")
+        cy.get(".invalid_input").should("have.text", "Invalid E-mail")
+    });
+
+    it("Invalid password", () => {
+        cy.get("#user").type("testing@testing.com")
+        cy.get("#password").type("1")
+        cy.get("#btnLogin").click()
+        cy.get(".invalid_input").should("be.visible")
+        cy.get(".invalid_input").should("have.text", "Invalid Password")
+    });
+});
