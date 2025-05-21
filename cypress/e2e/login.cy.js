3d65749af7a703ef211efdb68e29c7c8ac0b8371
Caroline Alves <e.carolinesalves@gmail.com>
2025-05-20 23:22:33 -0300

    Updating test case

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -11,7 +11,7 @@ describe("Login", () => {
         cy.get("#password").type("123456")
         cy.get("#btnLogin").click()
         cy.get(".swal2-title").should("be.visible")
-        cy.get(".swal2-title").should("have.text", "Login Ok")
+        cy.get(".swal2-title").should("have.text", "Login realizado")
     });
 
     it("Invalid email", () => {
